8baf2f4678af542a5dbb839a74a219deb1534752
leevtori <vlee2@ualberta.ca>
2019-01-10 19:03:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
 import requests
 print(requests.__version__)
 
+r = requests.get(‘https://raw.githubusercontent.com/leevtori/404-lab1/master/main.py')
+#print(r)
+#print(dir(r))
+print(r.text)
+print(r.status_code)
